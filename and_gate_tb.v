f99b92437fc1bb102465aec954805fc2ade6f78e
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-02-25 21:39:08 +0530

    DAY1

---
+++ and_gate_tb.v
@@ -0,0 +1,17 @@
+module and_gatetb;
+  reg a,b;
+  wire y;
+  
+  and_gate uut(.a(a),.b(b),.y(y));
+  
+  initial begin
+    $display("A B | Y");
+    a=0;b=0;#10 $display("%b %b | %b",a,b,y);
+    a=0;b=1;#10 $display("%b %b | %b",a,b,y);
+    a=1;b=0;#10 $display("%b %b | %b",a,b,y);
+    a=1;b=1;#10 $display("%b %b | %b",a,b,y);
+    $finish;
+  end
+endmodule
+  
+ 
