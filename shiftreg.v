f85b7dddefe17b5f49378c72599e4bb473f80bfb
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-08 16:08:58 +0530

    Day12
    added sipo module

---
+++ shiftreg.v
@@ -0,0 +1,36 @@
+module siso(
+  input clk,
+input rst,
+input si,
+  output s0);
+  reg [3:0] sreg;
+  
+  assign s0=sreg[0];
+  
+  always @(posedge clk or posedge rst)begin
+    if(rst)
+      sreg<=4'b0000;
+    else
+      sreg<={si,sreg[3:1]};
+  end
+endmodule
+
+
+
+// SIPO //
+module sipo(
+  input clk,
+input rst,
+input si,
+  output [3:0] s0);
+  reg [3:0] sreg;
+  
+  assign s0=sreg;
+  
+  always @(posedge clk or posedge rst)begin
+    if(rst)
+      sreg<=4'b0000;
+    else
+      sreg<={si,sreg[3:1]};
+  end
+endmodule
