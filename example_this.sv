23b3a6b64feb6271eb5e3a1dcc4701800d7b41cc
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-18 22:16:52 +0530

    day41

---
+++ example_this.sv
@@ -0,0 +1,9 @@
+class example;
+  string name;
+  int age;
+  
+  function void set_data(string name , int age);
+    this.name=name; //this.name is class name and is assigned to name of function
+    this.age=age;
+  endfunction
+endclass
