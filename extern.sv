82c34e7e0ca580b2a60ff2d6bf9365da46078e63
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-21 23:46:15 +0530

    Day46

---
+++ extern.sv
@@ -0,0 +1,27 @@
+class packet #(parameter WIDTH=8);
+  bit [WIDTH-1:0] data;
+
+  extern function void display();
+  endfunction
+endclass
+
+function void packet::display();
+  $display("Packet width =%0d , data=%0d",WIDTH,data);
+endfunction
+
+module param_class_example;
+  packet #(4) p1;
+  packet #(8) p2;
+
+  initial begin
+    p1=new();
+    p2=new();
+
+    p1.data=4'b1010;
+    p2.data=8'b11001100;
+
+    p1.display();
+    p2.display();
+  end
+endmodule
+    
