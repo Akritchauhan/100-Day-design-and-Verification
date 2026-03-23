78ca004a73f316f4df5aa6183796ec4918da882f
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-24 00:09:11 +0530

    Day52

---
+++ sem_exaple.sv
@@ -0,0 +1,20 @@
+module sem_example;
+
+  semaphore sem;
+  int q[$];
+  task producer();
+    int i;
+    for(i=0;i<5;i++)begin
+      q.push_back(i);
+      $display("[Producer] Produced =%0d",i);
+      sem.put(1);
+    end
+  endtask
+
+
+  task consumer();
+    forever begin
+      sem.get(1);
+      $display("[consumer] Consumed =%0d",q.pop_front());
+    end
+  endtask
