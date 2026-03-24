25f63902b8914aeb0218c98c135c288303d03e2b
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-03-24 22:54:18 +0530

    Day53

---
+++ mail_class.sv
@@ -0,0 +1,27 @@
+class packet;
+  int addr;
+  int data;
+endclass
+
+module mailbox_packet_ex;
+  mailbox mbx=new();
+
+  task producer();
+    packet p;
+    repeat(5) begin
+      p=new();
+      p.addr=$urandom_range(0,255);
+      p.data=$urandom_range(0,1023);
+      $display("[PRODUCER} Sending packet : addr=%0d data=%0d",p.addr,p.data);
+      mbx.put(p);
+    end
+  endtask
+
+  task consumer();
+    packet p;
+    forever begin
+      mbx.get(p);
+      $display("[CONSUMER] Reecived packet : addr=%0d data=%0d",p.addr,p.data);
+      #3;
+    end
+  endtask
